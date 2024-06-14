5514cd469f6a33ba9cbf0d9431809458da995632
Vlad <SpaceOCWindowsPROYT@gmail.com>
2024-06-14 13:41:43 +0700

    .gitignore update, create CMakeLists.txt, include and tuc.h code update

---
+++ include/tuc.h
@@ -0,0 +1,18 @@
+#include <string>
+#include "crow_all.h"
+
+#define TUC_VERSION "0.1 Alpha"
+#define TUC_COMMIT "?" // 
+
+class shell {
+    private:
+        crow::SimpleApp crow_app;
+    public:
+        void start() {
+            crow_app.port(18080).multithreaded().run();
+        }
+        void start(int port) {
+            crow_app.port(port).multithreaded().run();
+        }
+        void start(std::string ip, int port); // ???
+};
