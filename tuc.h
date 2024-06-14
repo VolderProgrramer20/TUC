a39d0cdac7a600e2ca3132b015673e154ddea7e7
Vlad <SpaceOCWindowsPROYT@gmail.com>
2024-06-14 12:23:21 +0700

    tuc.h code

--- tuc.h
+++ tuc.h
@@ -1,1 +1,12 @@
+#include <string>
 
+#define TUC_VERSION "0.1 Alpha"
+#define TUC_COMMIT "?" // 
+
+class shell {
+    private:
+    public:
+        void start();
+        void start(int port);
+        void start(std::string ip, int port);
+};
