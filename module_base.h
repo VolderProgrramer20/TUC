494849bf55add2b3f5f86511cc7f90e97e5d49f1
SpaceOC <141331688+SpaceOC@users.noreply.github.com>
2025-06-24 20:00:04 +0700

    Create module_base.h
    там смифно

---
+++ module_base.h
@@ -0,0 +1,13 @@
+// Х... типо смешно хаха ибо кто-то решил добавить новый файл сюда спустя год. Шутку поймут только единицы. (Автор коммита не считае-)
+// И вообще, а почему репозиторий не архивирован?
+
+class shell;
+
+class module_base {
+  public:
+  module_base(shell* core);
+  virtual void onLoad() = 0;
+  virtual void onUnload() = 0;
+  private:
+  shell* _corePtr;
+};
